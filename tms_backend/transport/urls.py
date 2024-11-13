2733a923286ae6bf182ef53584505bfff9e3c172
Siddhant Purkayastha <sidpurk04@gmail.com>
2024-11-13 11:12:26 +0530

    Update urls.py

--- tms_backend/transport/urls.py
+++ tms_backend/transport/urls.py
@@ -1,18 +1,12 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    CustomerViewSet, VehicleViewSet, DriverViewSet,
-    RouteViewSet, OrderViewSet, ShipmentViewSet
-)
-
-router = DefaultRouter()
-router.register(r'customers', CustomerViewSet)
-router.register(r'vehicles', VehicleViewSet)
-router.register(r'drivers', DriverViewSet)
-router.register(r'routes', RouteViewSet)
-router.register(r'orders', OrderViewSet)
-router.register(r'shipments', ShipmentViewSet)
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('register', views.register_user, name='register'),
+    path('login', views.user_login, name='login'),
+    path('add-orders', views.create_order, name='create-order'),
+    # path('shipments/track/<str:shipment_id>/', views.track_shipment, name='track-shipment'),
+    path('shipment-tracking', views.track_shipment, name='track-shipments'),
+    path('add-driver', views.add_driver, name='add-driver'),
+    path('add-vehicle', views.add_vehicle, name='add-vehicle'),
 ]
