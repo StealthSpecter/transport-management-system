fb19a9b7616811bb928d89ef6dc3ec8ea537ccbb
Siddhant Purkayastha <sidpurk04@gmail.com>
2024-11-13 11:12:03 +0530

    Update serializers.py

--- tms_backend/transport/serializers.py
+++ tms_backend/transport/serializers.py
@@ -1,11 +1,18 @@
-# transport/serializers.py
 from rest_framework import serializers
-from .models import Customer, Vehicle, Driver, Route, Order, Shipment
+from .models import Customer, Vehicle, Driver, Order, Shipment
 
 class CustomerSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['cust_id', 'cust_name', 'cust_address', 'cust_contact', 'cust_email', 'password']
+    
+    def create(self, validated_data):
+        customer = Customer(**validated_data)
+        customer.set_password(validated_data['password'])
+        customer.save()
+        return customer
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,17 +24,54 @@ class DriverSerializer(serializers.ModelSerializer):
         model = Driver
         fields = '__all__'
 
-class RouteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Route
-        fields = '__all__'
-
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
-class ShipmentSerializer(serializers.ModelSerializer):
+class ShipmentTrackingSerializer(serializers.ModelSerializer):
+    customer = serializers.SerializerMethodField()
+    vehicle = serializers.SerializerMethodField()
+    driver = serializers.SerializerMethodField()
+    order = serializers.SerializerMethodField()
+    
     class Meta:
         model = Shipment
         fields = '__all__'
+    
+    def get_customer(self, obj):
+        return {
+            'name': obj.order.customer.cust_name,
+            'contact': obj.order.customer.cust_contact,
+            'address': obj.order.customer.cust_address
+        }
+    
+    def get_vehicle(self, obj):
+        if obj.vehicle:
+            return {
+                'type': obj.vehicle.v_type,
+                'id': obj.vehicle.vehicle_id,
+                'capacity': obj.vehicle.v_capacity
+            }
+        return None
+    
+    def get_driver(self, obj):
+        if obj.driver:
+            return {
+                'name': obj.driver.d_name,
+                'id': obj.driver.driver_id,
+                'contact': obj.driver.d_contact
+            }
+        return None
+    
+    def get_order(self, obj):
+        return {
+            'order_id': obj.order.order_id,
+            'pickup_location': obj.order.o_pickup,
+            'delivery_location': obj.order.o_destination,
+            'weight': obj.order.weight,
+            'dimensions': obj.order.dimensions,
+            'type': obj.order.delivery_type,
+            # 'special_instructions': obj.order.special_instructions,
+            # 'expected_delivery': obj.order.expected_delivery
+        }
